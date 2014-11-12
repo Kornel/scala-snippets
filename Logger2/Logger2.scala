7547fb8e92f02589892b0e4dd620f15c7a122f03
Kornel Kielczewski <kornel.kielczewski@allegro.pl>
2014-11-12 22:55:41 +0100

    Does it get any worse than this?

--- Logger2/Logger2.scala
+++ Logger2/Logger2.scala
@@ -1,27 +1,33 @@
 
 object Logger2Demo extends App {
- 
-  val uu = for {
-    v1 <- Logger(10)
-    v2 <- Logger(2)
+
+  val uu2 = for {
+    v1 <- Logger("got 10", 10)
+    v2 <- Logger("got 2", 2)
   } yield v1 + v2
 
-  println(uu)
+  println(uu2)
+
 }
 
-class Logger[T](v: T) {
+class Logger[T](val v: (List[String], T)) {
 
-  def flatMap[R](f: (T) => Logger[R]): Logger[R] = f(v)
+  def flatMap[R](f: (T) => Logger[R]): Logger[R] = {
+    val a = f(v._2)
+    val lp = a.v._1
+    val vp = a.v._2
+    new Logger(lp ++ v._1, vp)
+  }
 
-  def map[R](f: (T) => R): Logger[R] = flatMap(x => Logger(f(x)))
+  def map[R](f: (T) => R): Logger[R] = flatMap(x => new Logger(Nil, f(x)))
 
-  override def toString = s"Logger($v)"
+  override def toString = s"$v"
 
 }
 
 object Logger {
   
-  def apply[T](v: T) = new Logger(v)
+  def apply[T](log: String, v: T) = new Logger((List(log), v))
 
 }
 
